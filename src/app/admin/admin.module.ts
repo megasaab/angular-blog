e1f1b9f12b8b3eba4d85f0ee303f611d84ea5e47
Sabyrzhan <a.sabyrzhan@kazdream.kz>
2020-08-23 02:22:38 +0600

    add auth services

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,8 @@ import {DashboardPageComponent} from './dashboard-page/dashboard-page.component'
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "./shared/services/auth.service";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     CommonModule,
       FormsModule,
       ReactiveFormsModule,
+      SharedModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
@@ -32,7 +35,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
       }
     ])
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AdminModule {
 
